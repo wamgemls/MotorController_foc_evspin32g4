0dab4c7daa3e6c2b0890d58de704d78725721415
MaxSp <wamge>
2021-12-22 22:47:28 +0100

    README.md_push

--- sensorless/STM32CubeIDE/Debug/Application/Startup/subdir.mk
+++ sensorless/STM32CubeIDE/Debug/Application/Startup/subdir.mk
@@ -18,10 +18,3 @@ S_DEPS += \
 Application/Startup/%.o: ../Application/Startup/%.s Application/Startup/subdir.mk
 	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
 
-clean: clean-Application-2f-Startup
-
-clean-Application-2f-Startup:
-	-$(RM) ./Application/Startup/startup_stm32g431vbtx.d ./Application/Startup/startup_stm32g431vbtx.o
-
-.PHONY: clean-Application-2f-Startup
-
